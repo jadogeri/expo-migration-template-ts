89d03046a191aafbce5756e9415f04294bfd7793
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-12-03 11:40:49 -0600

    Updated screens

--- app/src/screens/Explore.tsx
+++ app/src/screens/Explore.tsx
@@ -32,14 +32,14 @@ export default function ExploreScreen() {
       </ThemedView>
       
       <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
+      <Collapsible title="Non File-based routing">
         <ThemedText>
           This app has two screens:{' '}
           <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
           <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
         </ThemedText>
         <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
+          The tab layout file in <ThemedText type="defaultSemiBold">app/navigation/TabNavigator.tsx</ThemedText>{' '}
           sets up the tab navigator.
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
