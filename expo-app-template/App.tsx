7a0fda02ab5fc8fd3a764db311001a1d0ffc2f64
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-03 07:57:02 -0600

    added expo app and template

---
+++ expo-app-template/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RootStackNavigator from '@/navigation/RootStackNavigator'
+
+const App = () => {
+  return (
+ <RootStackNavigator />
+  )
+}
+
+export default App
+
+
